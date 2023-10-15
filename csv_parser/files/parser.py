acc7f5c23573cd0469d0145dd8b73efd172857c8
Daniil Balabanov <spacewalrus@bk.ru>
2023-10-15 03:18:41 +0300

    make filter

--- csv_parser/files/parser.py
+++ csv_parser/files/parser.py
@@ -50,9 +50,68 @@ def df_to_html(pk: int = None, df=None):
         return df.to_html()
 
 
-def filter_data(pk: int, columns_list: list[str]):
+def filter_data(pk: int,
+                columns_to_show: list[str],
+                columns_to_sort: list[str],
+                is_ascending: bool,
+                is_index: bool,
+                is_head: bool,
+                num_of_rows: int):
+
     file_object = Files.objects.get(id=pk)
     file_object_df = read_csv(file_object.file)
-    filtered_columns_df = file_object_df[columns_list]
-    return filtered_columns_df
+    filtered_data = sort_and_filter(file_object_df,
+                                    columns_to_show,
+                                    columns_to_sort,
+                                    is_ascending,
+                                    is_index,
+                                    is_head,
+                                    num_of_rows)
+    return filtered_data
 
+
+def sort_and_filter(df,
+                    cols_to_show=None,
+                    cols_to_sort=None,
+                    is_ascending=False,
+                    is_index=False,
+                    is_head=None,
+                    num_of_rows=None):
+
+    if cols_to_sort:
+        modified_df = df.sort_values(by=cols_to_sort,
+                                     ascending=is_ascending)
+        filtered_df = sort_and_filter(modified_df,
+                                      cols_to_show,
+                                      None,
+                                      is_ascending,
+                                      is_index,
+                                      is_head,
+                                      num_of_rows)
+        return filtered_df
+
+    if cols_to_show:
+        modified_df = df[cols_to_show]
+        filtered_df = sort_and_filter(modified_df,
+                                      is_ascending=is_ascending,
+                                      is_index=is_index,
+                                      is_head=is_head,
+                                      num_of_rows=num_of_rows)
+        return filtered_df
+
+    if is_index:
+        modified_df = df.sort_index(ascending=is_ascending)
+        filtered_df = sort_and_filter(modified_df,
+                                      is_head=is_head,
+                                      num_of_rows=num_of_rows)
+        return filtered_df
+
+    match is_head:
+        case 'True':
+            return df.head(abs(num_of_rows))
+        case 'False':
+            return df.tail(abs(num_of_rows))
+        case _:
+            pass
+
+    return df
