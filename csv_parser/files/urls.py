8e26d3576779efe87ad90615fadd88d4c6352c63
Daniil Balabanov <spacewalrus@bk.ru>
2023-10-16 21:59:30 +0300

    fix imports, add 3 new paths

--- csv_parser/files/urls.py
+++ csv_parser/files/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from csv_parser.files.views import upload_csv, FilesIndexView, FileDetailView, show_data
+from csv_parser.files.views import (
+    upload_csv,
+    FilesIndexView,
+    FileDetailView,
+    FileDeleteView,
+    show_data,
+    download_file
+)
 
 
 urlpatterns = [
     path('upload/', upload_csv, name="upload"),
     path('list/', FilesIndexView.as_view(), name="index"),
     path('<int:pk>/', FileDetailView.as_view(), name="file_details"),
-    path('<int:pk>/data', show_data, name='show_frame')
+    path('<int:pk>/data', show_data, name='show_frame'),
+    path('<int:pk>/download', download_file, name='download_file'),
+    path('<int:pk>/delete', FileDeleteView.as_view(), name='delete_file'),
 ]
